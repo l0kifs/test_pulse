96ec17b64cf58d45b46c87ac9ff11e110b5650c2
Sergei Konovalov <l0kifs91@gmail.com>
2024-12-08 14:56:10 +0600

    Refactor PrometheusAPI to use base_url instead of url in constructor

--- app/clients/prometheus_api.py
+++ app/clients/prometheus_api.py
@@ -2,9 +2,9 @@ import requests
 
 
 class PrometheusAPI:
-    def __init__(self, url: str):
-        self.url = url
+    def __init__(self, base_url: str):
+        self.base_url = base_url
 
     def get_metrics(self, query: str) -> dict:
-        response = requests.get(f"{self.url}/api/v1/query", params={"query": query})
+        response = requests.get(f"{self.base_url}/api/v1/query", params={"query": query})
         return response.json()
